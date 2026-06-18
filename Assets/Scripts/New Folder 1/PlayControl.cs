4f94c314b83a19e210e9e429d8805871e1199639
bitch <169540526+aspiringbitch@users.noreply.github.com>
2026-06-18 22:07:09 +0530

    #2

--- Assets/Scripts/New Folder 1/PlayControl.cs
+++ Assets/Scripts/New Folder 1/PlayControl.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayControl : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public float speed = 10.0f;
+    public float turnSpeed;
+    public InputAction moveAction;
     void Start()
     {
         
@@ -14,5 +18,7 @@ public class PlayControl : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+
     }
 }
