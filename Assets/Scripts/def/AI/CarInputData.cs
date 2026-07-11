42aa58227971501a669d702cd7cbc89974e09d45
bitch <169540526+follicallyChallenged@users.noreply.github.com>
2026-07-11 15:00:49 +0530

    web-cam

---
+++ Assets/Scripts/def/AI/CarInputData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+public class CarInputData
+{
+    public float throttle;
+    public float brake;
+    public float steering;
+    public bool nitro;
+}
---
+++ Assets/Scripts/def/AI/ICarInputProvider.cs
@@ -0,0 +1,4 @@
+public interface ICarInputProvider
+{
+    CarInputData GetInput();
+}
